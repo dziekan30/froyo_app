a0017a430f3309376c1c8d6ddf68342d77faeb27
tommy3wood <thomaswcurran1014@gmail.com>
2020-02-22 17:54:22 -0600

    generated models

---
+++ app/models/event.rb
@@ -0,0 +1,9 @@
+class Event < ApplicationRecord
+  belongs_to :user
+
+  validates :user_id, presence: true
+  validates :name, presence: true
+  validates :category, presence: true
+  validates :start_time, presence: true
+  validates :end_time, presence: true
+end
